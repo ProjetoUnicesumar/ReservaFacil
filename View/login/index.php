c786c9061065c2a695b91a23e83abd8dd69ea0b4
Vinicius G. Bueno <viniciusgoncalvesbueno1@gmail.com>
2025-03-18 21:41:23 -0300

    Adicionei um botão de cadastro

--- View/login/index.php
+++ View/login/index.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../../ConexaoBD/Conexao.php';
     <div class="form-box">
             <h2>Entrar na minha Conta</h2>
            
-            <form action="#Enivar para o banco">
+            <form action="#Enviar para o banco">
                 <div class="input-group">
                     <label for="email">E-mail</label>
                     <input type="email" id="email" placeholder="Digite o seu email" required>
@@ -30,8 +30,9 @@ require_once __DIR__ . '/../../ConexaoBD/Conexao.php';
                     <button>Entrar</button>
                 </div>
 
-                <p> Cadastrar como passageiro: <a href="/ReservaFacil/View/CadastroUsuario/cadastroUsuario.php" > Cadastrar </a> </p>
-
+                <div class ="input-group">
+                    <button class="cadastrar">Cadastrar como Passageiro</button>
+                </div>
     </div>
 </body>
 </html>
